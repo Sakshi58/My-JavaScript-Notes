3f04873f72cc6b29d3601c344886e2e65555204d
Sakshi Singh <sakshisingh.ss58@gmail.com>
2023-10-21 12:13:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+document.writeln("This is DOM manipulation");
+document.writeln("hello people");                     //to write  on the web page
+console.warn("this is a  warning");                   // to create warnings in the console
+console.error("this will create  errors in the  console");      //to create  errors
+var x1 =10;
+var  x2= 20;
+var res = x1+x2;
+console.log(res);
+document.writeln(res);
+
+var num1 = 23;
+var num2= "hello";
+console.log(num1+num2);
+
+//object data type
+var marks= {
+    ravi: 80.5,
+    teza: 56,
+    sam:90.433,
+    tom: 67.778
+}
+
+var und;
+console.log(und);
+document.write(und);
+console.log(45+und);
